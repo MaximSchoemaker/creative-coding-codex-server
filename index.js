1887806363d373912e3a8024acc365e55f648eff
Maxim Schoemaker <maxim.schoemaker@gmail.com>
2021-03-28 01:33:04 +0100

    asdf

--- index.js
+++ index.js
@@ -19,10 +19,12 @@ var cookieParser = require('cookie-parser');
 const { MongoClient, ObjectId } = require('mongodb');
 
 const PUBLIC_DOMAIN = "maximschoemaker.com";
+const PUBLIC_PATH = "creative-coding-codex/";
 const PUBLIC_URL = "http://188.226.142.229:3001";
 const FRONTEND_PUBLIC_URL = "http://maximschoemaker.com/creative-coding-codex";
 
 // const PUBLIC_DOMAIN = "localhost";
+// const PUBLIC_PATH = "/";
 // const PUBLIC_URL = "http://localhost:3001";
 // const FRONTEND_PUBLIC_URL = "http://localhost:3000/";
 
@@ -40,7 +42,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json())
 app.use(session({
   secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true,
-  cookie: { domain: PUBLIC_DOMAIN },
+  cookie: { domain: PUBLIC_DOMAIN, path: PUBLIC_PATH },
 }));
 app.use(passport.initialize());
 app.use(passport.session());
